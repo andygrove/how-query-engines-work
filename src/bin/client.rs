8ebe42a58a4f48de6b9ba64e5bbb402229e85bae
Andy Grove <andygrove73@gmail.com>
2019-07-04 14:19:22 -0600

    uncomment code

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,53 +1,53 @@
-//#![deny(warnings, rust_2018_idioms)]
-//
-//use futures::Future;
-//use hyper::client::connect::{Destination, HttpConnector};
-//use tower_grpc::Request;
-//use tower_hyper::{client, util};
-//use tower_util::MakeService;
-//
-//pub mod hello_world {
-//    include!(concat!(env!("OUT_DIR"), "/helloworld.rs"));
-//}
-//
+#![deny(warnings, rust_2018_idioms)]
+
+use futures::Future;
+use hyper::client::connect::{Destination, HttpConnector};
+use tower_grpc::Request;
+use tower_hyper::{client, util};
+use tower_util::MakeService;
+
+pub mod hello_world {
+    include!(concat!(env!("OUT_DIR"), "/helloworld.rs"));
+}
+
 pub fn main() {
-//    let _ = ::env_logger::init();
-//
-//    let uri: http::Uri = format!("http://[::1]:50051").parse().unwrap();
-//
-//    let dst = Destination::try_from_uri(uri.clone()).unwrap();
-//    let connector = util::Connector::new(HttpConnector::new(4));
-//    let settings = client::Builder::new().http2_only(true).clone();
-//    let mut make_client = client::Connect::with_builder(connector, settings);
-//
-//    let say_hello = make_client
-//        .make_service(dst)
-//        .map_err(|e| panic!("connect error: {:?}", e))
-//        .and_then(move |conn| {
-//            use crate::hello_world::client::Greeter;
-//
-//            let conn = tower_request_modifier::Builder::new()
-//                .set_origin(uri)
-//                .build(conn)
-//                .unwrap();
-//
-//            // Wait until the client is ready...
-//            Greeter::new(conn).ready()
-//        })
-//        .and_then(|mut client| {
-//            use crate::hello_world::HelloRequest;
-//
-//            client.say_hello(Request::new(HelloRequest {
-//                name: "What is in a name?".to_string(),
-//            }))
-//        })
-//        .and_then(|response| {
-//            println!("RESPONSE = {:?}", response);
-//            Ok(())
-//        })
-//        .map_err(|e| {
-//            println!("ERR = {:?}", e);
-//        });
-//
-//    tokio::run(say_hello);
+    let _ = ::env_logger::init();
+
+    let uri: http::Uri = format!("http://[::1]:50051").parse().unwrap();
+
+    let dst = Destination::try_from_uri(uri.clone()).unwrap();
+    let connector = util::Connector::new(HttpConnector::new(4));
+    let settings = client::Builder::new().http2_only(true).clone();
+    let mut make_client = client::Connect::with_builder(connector, settings);
+
+    let say_hello = make_client
+        .make_service(dst)
+        .map_err(|e| panic!("connect error: {:?}", e))
+        .and_then(move |conn| {
+            use crate::hello_world::client::Greeter;
+
+            let conn = tower_request_modifier::Builder::new()
+                .set_origin(uri)
+                .build(conn)
+                .unwrap();
+
+            // Wait until the client is ready...
+            Greeter::new(conn).ready()
+        })
+        .and_then(|mut client| {
+            use crate::hello_world::HelloRequest;
+
+            client.say_hello(Request::new(HelloRequest {
+                name: "What is in a name?".to_string(),
+            }))
+        })
+        .and_then(|response| {
+            println!("RESPONSE = {:?}", response);
+            Ok(())
+        })
+        .map_err(|e| {
+            println!("ERR = {:?}", e);
+        });
+
+    tokio::run(say_hello);
 }
