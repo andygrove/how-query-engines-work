c24334c472b3d53c1310bf67a22f2afa2f2f166b
Andy Grove <andygrove73@gmail.com>
2019-07-04 14:15:39 -0600

    reserve name on crates.io

--- build.rs
+++ build.rs
@@ -1,31 +1,10 @@
 fn main() {
-
-    println!("Running build.rs ...");
-
-    // Build helloworld
-    tower_grpc_build::Config::new()
-        .enable_server(true)
-        .enable_client(true)
-        .build(
-            &["proto/ballista/ballista.proto"],
-            &["proto/ballista"],
-        )
-        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
-
-//    // Build metadata
-//    tower_grpc_build::Config::new()
-//        .enable_server(true)
-//        .enable_client(true)
-//        .build(&["proto/metadata/metadata.proto"], &["proto/metadata"])
-//        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
-//
-//    // Build routeguide
 //    tower_grpc_build::Config::new()
 //        .enable_server(true)
 //        .enable_client(true)
 //        .build(
-//            &["proto/routeguide/route_guide.proto"],
-//            &["proto/routeguide"],
+//            &["proto/ballista/ballista.proto"],
+//            &["proto/ballista"],
 //        )
 //        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,60 +1,60 @@
-#![deny(warnings, rust_2018_idioms)]
-
-use crate::hello_world::{server, HelloReply, HelloRequest};
-
-use futures::{future, Future, Stream};
-use log::error;
-use tokio::net::TcpListener;
-use tower_grpc::{Request, Response};
-use tower_hyper::server::{Http, Server};
-
-pub mod hello_world {
-    include!(concat!(env!("OUT_DIR"), "/helloworld.rs"));
-}
-
-#[derive(Clone, Debug)]
-struct Greet;
-
-impl server::Greeter for Greet {
-    type SayHelloFuture = future::FutureResult<Response<HelloReply>, tower_grpc::Status>;
-
-    fn say_hello(&mut self, request: Request<HelloRequest>) -> Self::SayHelloFuture {
-        println!("REQUEST = {:?}", request);
-
-        let response = Response::new(HelloReply {
-            message: "Zomg, it works!".to_string(),
-        });
-
-        future::ok(response)
-    }
-
-}
-
-pub fn main() {
-    let _ = ::env_logger::init();
-
-    let new_service = server::GreeterServer::new(Greet);
-
-    let mut server = Server::new(new_service);
-
-    let http = Http::new().http2_only(true).clone();
-
-    let addr = "[::1]:50051".parse().unwrap();
-    let bind = TcpListener::bind(&addr).expect("bind");
-
-    let serve = bind
-        .incoming()
-        .for_each(move |sock| {
-            if let Err(e) = sock.set_nodelay(true) {
-                return Err(e);
-            }
-
-            let serve = server.serve_with(sock, http.clone());
-            tokio::spawn(serve.map_err(|e| error!("hyper error: {:?}", e)));
-
-            Ok(())
-        })
-        .map_err(|e| eprintln!("accept error: {}", e));
-
-    tokio::run(serve)
-}
+//#![deny(warnings, rust_2018_idioms)]
+//
+//use crate::hello_world::{server, HelloReply, HelloRequest};
+//
+//use futures::{future, Future, Stream};
+//use log::error;
+//use tokio::net::TcpListener;
+//use tower_grpc::{Request, Response};
+//use tower_hyper::server::{Http, Server};
+//
+//pub mod hello_world {
+//    include!(concat!(env!("OUT_DIR"), "/helloworld.rs"));
+//}
+//
+//#[derive(Clone, Debug)]
+//struct Greet;
+//
+//impl server::Greeter for Greet {
+//    type SayHelloFuture = future::FutureResult<Response<HelloReply>, tower_grpc::Status>;
+//
+//    fn say_hello(&mut self, request: Request<HelloRequest>) -> Self::SayHelloFuture {
+//        println!("REQUEST = {:?}", request);
+//
+//        let response = Response::new(HelloReply {
+//            message: "Zomg, it works!".to_string(),
+//        });
+//
+//        future::ok(response)
+//    }
+//
+//}
+//
+//pub fn main() {
+//    let _ = ::env_logger::init();
+//
+//    let new_service = server::GreeterServer::new(Greet);
+//
+//    let mut server = Server::new(new_service);
+//
+//    let http = Http::new().http2_only(true).clone();
+//
+//    let addr = "[::1]:50051".parse().unwrap();
+//    let bind = TcpListener::bind(&addr).expect("bind");
+//
+//    let serve = bind
+//        .incoming()
+//        .for_each(move |sock| {
+//            if let Err(e) = sock.set_nodelay(true) {
+//                return Err(e);
+//            }
+//
+//            let serve = server.serve_with(sock, http.clone());
+//            tokio::spawn(serve.map_err(|e| error!("hyper error: {:?}", e)));
+//
+//            Ok(())
+//        })
+//        .map_err(|e| eprintln!("accept error: {}", e));
+//
+//    tokio::run(serve)
+//}
