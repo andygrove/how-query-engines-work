1275b9ca2da31ce7835ce94ceab4d300e7749332
Rick Richardson <rick.richardson@gmail.com>
2019-07-17 16:48:02 -0700

    refactored dockerfiles to make caches work despite source changes (#17)

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-docker build -t ballista .
+
+REPO=andygrove
+
+docker build -t ${REPO}/ballista-platform -f docker/Dockerfile.platform .
+docker build -t ${REPO}/ballista -f docker/Dockerfile.binary --build-arg REPO=${REPO} .
--- examples/nyctaxi/build-docker-image.sh
+++ examples/nyctaxi/build-docker-image.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-mkdir -p tmp/ballista/src
-mkdir -p tmp/ballista/proto
-cp -rf ../../Cargo.* tmp/ballista
-cp -rf ../../build.rs tmp/ballista
-cp -rf ../../src/* tmp/ballista/src
-cp -rf ../../proto/* tmp/ballista/proto
-docker build -t ballista-nyctaxi .
+
+REPO=andygrove
+
+docker pull andygrove/ballista-platform
+docker build -t ${REPO}/example-nyctaxi --build-arg REPO=${REPO} .
