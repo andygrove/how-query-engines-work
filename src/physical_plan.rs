bb1708ce2af41904a6e8ad76dc042cac1f84b367
Andy Grove <andygrove@users.noreply.github.com>
2019-07-26 09:40:07 -0600

    Refactor physical plan to use struct instead of enum (#54)

--- src/physical_plan.rs
+++ src/physical_plan.rs
@@ -1,62 +1,154 @@
 //! Ballista physical query plan
 
-use datafusion::logicalplan::Expr;
+use arrow::datatypes::Schema;
 
-/// Ballista physical query plan. Note that most of the variants here are just placeholders
-/// currently. Also note that this plan references Expr from the DataFusion logical plan
-/// and this may change in the future.
+pub trait PhysicalPlan {}
+
+/// Projection filters the input by column
 #[allow(dead_code)]
-#[derive(Debug, Clone)]
-enum PhysicalPlan {
-    /// Projection filters the input by column
-    Projection {
-        columns: Vec<usize>,
-        input: Box<PhysicalPlan>,
-    },
-    /// Selection filters the input by row
-    Selection {
-        filter: Expr,
-        input: Box<PhysicalPlan>,
-    },
-    /// GroupAggregate assumes inputs are ordered by the grouping expression and merges the
-    /// results without having to maintain a hash map
-    GroupAggregate {
-        group_expr: Vec<Expr>,
-        aggr_expr: Vec<Expr>,
-        input: Box<PhysicalPlan>,
-    },
-    /// HashAggregate assumes that the input is unordered and uses a hash map to maintain
-    /// accumulators per grouping hash
-    HashAggregate {
-        group_expr: Vec<Expr>,
-        aggr_expr: Vec<Expr>,
-        input: Box<PhysicalPlan>,
-    },
-    Sort {
-        sort_expr: Vec<Expr>,
-        input: Box<PhysicalPlan>,
-    },
-    /// Merge the input for each partition without additional processing and combine into a
-    /// single partition.
-    Merge { input: Box<PhysicalPlan> },
-    /// Represents a file scan
-    FileScan {
-        path: String,
-        projection: Vec<usize>,
-    },
+pub struct Projection {
+    columns: Vec<usize>,
+    input: Box<dyn PhysicalPlan>,
+}
+
+impl PhysicalPlan for Projection {}
+
+/// Selection filters the input by row
+#[allow(dead_code)]
+pub struct Selection {
+    filter: Box<dyn PhysicalExpr>,
+    input: Box<dyn PhysicalPlan>,
+}
+
+/// GroupAggregate assumes inputs are ordered by the grouping expression and merges the
+/// results without having to maintain a hash map
+#[allow(dead_code)]
+pub struct GroupAggregate {
+    group_expr: Vec<Box<dyn PhysicalExpr>>,
+    aggr_expr: Vec<Box<dyn PhysicalExpr>>,
+    input: Box<dyn PhysicalPlan>,
+}
+
+/// HashAggregate assumes that the input is unordered and uses a hash map to maintain
+/// accumulators per grouping hash
+#[allow(dead_code)]
+pub struct HashAggregate {
+    group_expr: Vec<Box<dyn PhysicalExpr>>,
+    aggr_expr: Vec<Box<dyn PhysicalExpr>>,
+    input: Box<dyn PhysicalPlan>,
+}
+
+impl HashAggregate {
+    pub fn new(
+        group_expr: Vec<Box<dyn PhysicalExpr>>,
+        aggr_expr: Vec<Box<dyn PhysicalExpr>>,
+        input: Box<dyn PhysicalPlan>,
+    ) -> Self {
+        Self {
+            group_expr,
+            aggr_expr,
+            input,
+        }
+    }
+}
+
+impl PhysicalPlan for HashAggregate {}
+
+/// Merge to a single partition
+#[allow(dead_code)]
+pub struct Merge {
+    input: Box<dyn PhysicalPlan>,
+}
+
+impl Merge {
+    pub fn new(input: Box<dyn PhysicalPlan>) -> Self {
+        Self { input }
+    }
+}
+
+impl PhysicalPlan for Merge {}
+
+/// Represents a partitioned file scan
+#[allow(dead_code)]
+pub struct FileScan {
+    path: Vec<String>,
+    schema: Box<Schema>,
+    projection: Vec<usize>,
+}
+
+impl FileScan {
+    pub fn new(path: Vec<String>, schema: Box<Schema>, projection: Vec<usize>) -> Self {
+        FileScan {
+            path,
+            schema,
+            projection,
+        }
+    }
+}
+
+impl PhysicalPlan for FileScan {}
+
+pub trait PhysicalExpr {}
+
+/// Column reference
+#[allow(dead_code)]
+pub struct Column {
+    index: usize,
+}
+
+impl Column {
+    pub fn new(index: usize) -> Self {
+        Self { index }
+    }
+}
+
+impl PhysicalExpr for Column {}
+
+pub fn col(index: usize) -> Box<Column> {
+    Box::new(Column::new(index))
+}
+
+/// MAX aggregate function
+#[allow(dead_code)]
+pub struct Max {
+    expr: Box<dyn PhysicalExpr>,
+}
+
+impl Max {
+    pub fn new(expr: Box<dyn PhysicalExpr>) -> Self {
+        Self { expr }
+    }
+}
+
+impl PhysicalExpr for Max {}
+
+pub fn max(expr: Box<dyn PhysicalExpr>) -> Box<Max> {
+    Box::new(Max::new(expr))
+}
+
+/// MIN aggregate function
+#[allow(dead_code)]
+pub struct Min {
+    expr: Box<dyn PhysicalExpr>,
+}
+
+impl Min {
+    pub fn new(expr: Box<dyn PhysicalExpr>) -> Self {
+        Self { expr }
+    }
+}
+
+impl PhysicalExpr for Min {}
+
+pub fn min(expr: Box<dyn PhysicalExpr>) -> Box<Min> {
+    Box::new(Min::new(expr))
 }
 
 #[cfg(test)]
 mod tests {
-
     use crate::error::Result;
-    use crate::physical_plan::PhysicalPlan::*;
+    use crate::physical_plan::*;
     use arrow::datatypes::{DataType, Field, Schema};
-    use datafusion::execution::context::ExecutionContext;
-    use datafusion::execution::table_impl::TableImpl;
-    use datafusion::logicalplan::LogicalPlan;
-    use datafusion::table::Table;
-    use std::sync::Arc;
 
     #[test]
     fn aggregate() -> Result<()> {
@@ -81,54 +173,19 @@ mod tests {
             Field::new("total_amount", DataType::Float64, true),
         ]);
 
-        // construct the HashAggregate that will run on each executor
-        let mut ctx = ExecutionContext::new();
-        ctx.register_csv("tripdata", "file.csv", &schema, true);
-        let table = ctx.table("tripdata")?;
-        let passenger_count = table.col("passenger_count")?;
-        let fare_amount = table.col("fare_amount")?;
-        let aggr = HashAggregate {
-            group_expr: vec![passenger_count],
-            aggr_expr: vec![table.min(&fare_amount)?, table.max(&fare_amount)?],
-            input: Box::new(FileScan {
-                path: "/data".to_string(),
-                projection: vec![3, 10],
-            }),
-        };
-
-        // next step is to merge the results into a single partition
-        let merge = Merge {
-            input: Box::new(aggr),
-        };
-
-        // finally we need to perform a new aggregate
-        let merge_schema = Schema::new(vec![
-            Field::new("passenger_count", DataType::Utf8, true),
-            Field::new("min_fare_amount", DataType::Float64, true),
-            Field::new("max_fare_amount", DataType::Float64, true),
-        ]);
-        let merge_logical_plan = LogicalPlan::TableScan {
-            schema_name: "".to_string(),
-            table_name: "".to_string(),
-            table_schema: Arc::new(merge_schema.clone()),
-            projected_schema: Arc::new(merge_schema),
-            projection: None,
-        };
-        let table = TableImpl::new(Arc::new(merge_logical_plan));
-
-        let final_aggr = HashAggregate {
-            group_expr: vec![table.col("passenger_count")?],
-            aggr_expr: vec![
-                table.min(&table.col("min_fare_amount")?)?,
-                table.max(&table.col("max_fare_amount")?)?,
-            ],
-            input: Box::new(merge),
-        };
-
-        println!("Plan: {:?}", final_aggr);
-        let expected = "HashAggregate { group_expr: [#0], aggr_expr: [MIN(#1), MAX(#2)], input: Merge { input: HashAggregate { group_expr: [#3], aggr_expr: [MIN(#10), MAX(#10)], input: FileScan { path: \"/data\", projection: [3, 10] } } } }";
-
-        assert_eq!(expected, format!("{:?}", final_aggr));
+        let _ = HashAggregate::new(
+            vec![col(0)],
+            vec![min(col(1)), max(col(1))],
+            Box::new(Merge::new(Box::new(HashAggregate::new(
+                vec![col(0)],
+                vec![min(col(1)), max(col(1))],
+                Box::new(FileScan::new(
+                    vec!["file1.csv".to_string(), "file2.csv".to_string()],
+                    Box::new(schema),
+                    vec![3, 10],
+                )),
+            )))),
+        );
 
         Ok(())
     }
