3f56deab0cb7f4ad8ac4e44e6b82ce2eab6415ab
Andy Grove <andygrove73@gmail.com>
2020-02-14 18:39:16 -0700

    remove dupe protobuf file

--- jvm/protobuf/build.gradle.kts
+++ jvm/protobuf/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     id("idea")
 }
 
+sourceSets {
+    main {
+        proto {
+            srcDir("../../proto")
+        }
+    }
+}
+
 dependencies {
     implementation("com.google.protobuf:protobuf-java:3.11.4")
     testImplementation("junit:junit:4.13")
