434e936ee9ad5eb72f51be53883583dfbfd60970
Andy Grove <andygrove73@gmail.com>
2020-04-21 06:40:35 -0600

    save

--- rust/build.rs
+++ rust/build.rs
@@ -1,10 +1,4 @@
 fn main() {
-    prost_build::compile_protos(&["../proto/ballista.proto"], &["../proto"])
+    prost_build::compile_protos(&["proto/ballista.proto"], &["proto"])
         .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
-
-    // tower_grpc_build::Config::new()
-    //     .enable_server(true)
-    //     .enable_client(true)
-    //     .build(&["../proto/ballista.proto"], &["../proto"])
-    //     .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
