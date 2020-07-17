3cae798f02e96bd39e5e590f9a59b99f44223c35
Andy Grove <andygrove@users.noreply.github.com>
2020-07-16 20:16:04 -0600

    Remove old Rust benchmarks that are no longer valid (#226)

--- dev/build-rust.sh
+++ dev/build-rust.sh
@@ -12,4 +12,3 @@ cargo test
 popd
 
 docker build -t ballistacompute/ballista-rust:$BALLISTA_VERSION -f docker/rust-executor.dockerfile .
-docker build -t ballistacompute/rust-benchmarks:$BALLISTA_VERSION -f docker/rust-benchmarks.dockerfile .
--- rust/benchmarks/run-dockerized.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# run via docker
-docker run --cpus=12 -e BENCH_RESULT_FILE="rust-dockerized.txt" -e BENCH_PATH="/mnt/nyctaxi" -e BENCH_FORMAT="csv" -e BENCH_ITERATIONS="3" -e BENCH_MODE="local" -v /mnt/nyctaxi/csv/year=2019:/mnt/nyctaxi -v /home/andy/git/andygrove/ballista/rust/benchmarks:/results ballistacompute/rust-benchmarks:0.2.5
