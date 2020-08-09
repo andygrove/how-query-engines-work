1c187d2bc801db6967c73997764a5cb4d2c8c574
Andy Grove <andygrove73@gmail.com>
2020-08-09 12:19:55 -0600

    bump version to 0.3.0

--- dev/bump-version.sh
+++ dev/bump-version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-OLD_VERSION=0.2.6-SNAPSHOT
-NEW_VERSION=0.3.0-SNAPSHOT
+OLD_VERSION=0.3.0-SNAPSHOT
+NEW_VERSION=0.3.0
 
 set -e
 
 find . -name Cargo.toml -exec sed -i -e "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
 find . -name build.gradle.kts -exec sed -i -e "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
+find . -name "*.md" -exec sed -i -e "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
+find . -name "*.yaml" -exec sed -i -e "s/$OLD_VERSION/$NEW_VERSION/g" {} \;
 find . -name "*.dockerfile" -exec sed -i -e "s/:$OLD_VERSION/:$NEW_VERSION/g" {} \;
 find . -name docker-compose.yml -exec sed -i -e "s/:$OLD_VERSION/:$NEW_VERSION/g" {} \;
 find dev -name "*.sh" -exec sed -i -e "s/BALLISTA_VERSION=$OLD_VERSION/BALLISTA_VERSION=$NEW_VERSION/g" {} \;
--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "org.ballistacompute"
-version = "0.3.0-SNAPSHOT"
+version = "0.3.0"
 
 allprojects {
     repositories {
